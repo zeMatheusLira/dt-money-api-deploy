d584c6fa5dbaa1861e310f9e62a0d75759b4159a
italocarlo06 <italocarlo06@gmail.com>
2026-04-30 20:57:42 -0300

    fix css

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
   
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, {
-    customCss: CSS_URL,
+    customCssUrl: CSS_URL,
   });
 
   
